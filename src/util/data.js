64e51e8d1b2c6600fd468b69988ffffd61881c5b
Suleman Rasheed <bcsf17m040@pucit.edu.pk>
2020-08-18 15:14:21 +0500

    Added data for services list

---
+++ src/util/data.js
@@ -0,0 +1,41 @@
+import detail from '../images/detail.svg'
+import consultation from '../images/consultation.svg'
+import emergency from '../images/emergency.svg'
+import pharmacy from '../images/pharmacy.svg'
+import search from '../images/search.svg'
+import tracking from '../images/tracking.svg'
+
+
+export const servicesList = [
+    {
+        icon: search,
+        heading: 'Search doctor',
+        title: 'Choose your doctor from thousands of specialist, general, and trusted hospitals',
+    },
+    {
+        icon: pharmacy,
+        heading: 'Online pharmacy',
+        title: 'Buy  your medicines with our mobile application with a simple delivery system',
+    },
+    {
+        icon: consultation,
+        heading: 'Consultation',
+        title: 'Free consultation with our trusted doctors and get the best recomendations',
+    },
+    {
+        icon: detail,
+        heading: 'Details info',
+        title: 'Free consultation with our trusted doctors and get the best recomendations',
+    },
+    {
+        icon: emergency,
+        heading: 'Emergency care',
+        title: `You can get 24/7 urgent care for yourself or your children and your
+        lovely family`,
+    },
+    {
+        icon: tracking,
+        heading: 'Tracking',
+        title: 'Track and save your medical history and health data ',
+    },
+]
